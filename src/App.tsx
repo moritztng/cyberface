c15aed1869ec5ab01fd2eec6d61ab40fcbc06bc0
Moritz Thüning <moritz.thuening@gmail.com>
2022-04-19 12:04:39 +0200

    Add speech2face and graphics to synthesizer

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,6 @@ interface Settings {
   music: string;
 }
 
-const sceneToPort = {'space': '5001', 'fluid': '5002'};
-
 async function query(urlString: string, params: { [key: string]: string; }) {
   const url = new URL(urlString);
   for (const [key, value] of Object.entries(params)) {
@@ -33,21 +31,21 @@ function App() {
     setAudioUrl('');
     setAnimationUrl('');
     setAnimating(true);
-    const speechId = await query('https://europe-west1-synthesizer-337314.cloudfunctions.net/speech', {'ssml': script, 'voice': settings.voice, 'rate': settings.speed, 'pitch': settings.pitch});
-    let url = await query('https://europe-west1-synthesizer-337314.cloudfunctions.net/music', {'id': speechId, 'music': settings.music, 'volume': settings.volume});
+    const speechId = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/speech', {'ssml': script, 'voice': settings.voice, 'rate': settings.speed, 'pitch': settings.pitch});
+    let url = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/music', {'id': speechId, 'music': settings.music, 'volume': settings.volume});
     setAudioUrl(url);
     const musicId = url.split('synthesizer-music/')[1].split('.')[0];
-    const animateId = await query('http://34.147.22.144:5000/animate', {'id': speechId});
-    const renderId = await query(`http://34.147.22.144:${sceneToPort[settings.scene]}/render`, {'id': animateId});
-    url = await query('https://europe-west1-synthesizer-337314.cloudfunctions.net/audio', {'audio-id': musicId, 'video-id': renderId});
+    const speech2faceId = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/speech2face', {'id': speechId});
+    const graphicsId = await query(`https://synthesizer-ukp4sgtskq-ez.a.run.app/graphics`, {'id': speech2faceId, 'scene': settings.scene});
+    url = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/audio', {'audio-id': musicId, 'video-id': graphicsId});
     setAnimationUrl(url);
     setAnimating(false);
   }
   async function listen(script: string, settings: Settings) {
     setAudioUrl('');
     setSpeaking(true);
-    const id = await query('https://europe-west1-synthesizer-337314.cloudfunctions.net/speech', {'ssml': script, 'voice': settings.voice, 'rate': settings.speed, 'pitch': settings.pitch});
-    const url = await query('https://europe-west1-synthesizer-337314.cloudfunctions.net/music', {'id': id, 'music': settings.music, 'volume': settings.volume});
+    const id = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/speech', {'ssml': script, 'voice': settings.voice, 'rate': settings.speed, 'pitch': settings.pitch});
+    const url = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/music', {'id': id, 'music': settings.music, 'volume': settings.volume});
     setAudioUrl(url);
     setSpeaking(false);
     new Audio(url).play();
