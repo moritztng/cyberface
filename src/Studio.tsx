75855cba1a6710135826c78a9c252b0d1834be12
Moritz Thüning <moritz.thuening@gmail.com>
2022-04-23 14:39:25 +0200

    Change audioUrl to audio

--- src/Studio.tsx
+++ src/Studio.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Studio.css';
 import Header from './Header';
 import Body from './Body';
@@ -23,17 +23,17 @@ async function query(urlString: string, params: { [key: string]: string; }) {
 }
 
 function Studio() {
-  const [audioUrl, setAudioUrl] = useState('https://storage.googleapis.com/synthesizer-music/262fa7573d25da1772e11892f91c77f7b4484f23e1d79d54c7011cf43656ab22.mp3');
+  const audio = useRef(new Audio('https://storage.googleapis.com/synthesizer-music/262fa7573d25da1772e11892f91c77f7b4484f23e1d79d54c7011cf43656ab22.mp3'));
   const [animationUrl, setAnimationUrl] = useState('https://storage.googleapis.com/synthesizer-audio/d430b368a858e5f541b5acf899d8ce95759046d956c2a157ab6354afecb031dd.mp4');
   const [speaking, setSpeaking] = useState(false);
   const [animating, setAnimating] = useState(false);
   async function animate(script: string, settings: Settings) {
-    setAudioUrl('');
+    audio.current.src = '';
     setAnimationUrl('');
     setAnimating(true);
     const speechId = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/speech', {'ssml': script, 'voice': settings.voice, 'rate': settings.speed, 'pitch': settings.pitch});
     let url = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/music', {'id': speechId, 'music': settings.music, 'volume': settings.volume});
-    setAudioUrl(url);
+    audio.current.src = url;
     const musicId = url.split('synthesizer-music/')[1].split('.')[0];
     const speech2faceId = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/speech2face', {'id': speechId});
     const graphicsId = await query(`https://synthesizer-ukp4sgtskq-ez.a.run.app/graphics`, {'id': speech2faceId, 'scene': settings.scene});
@@ -42,13 +42,12 @@ function Studio() {
     setAnimating(false);
   }
   async function listen(script: string, settings: Settings) {
-    setAudioUrl('');
+    audio.current.src = '';
     setSpeaking(true);
     const id = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/speech', {'ssml': script, 'voice': settings.voice, 'rate': settings.speed, 'pitch': settings.pitch});
-    const url = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/music', {'id': id, 'music': settings.music, 'volume': settings.volume});
-    setAudioUrl(url);
+    audio.current.src = await query('https://synthesizer-ukp4sgtskq-ez.a.run.app/music', {'id': id, 'music': settings.music, 'volume': settings.volume});
     setSpeaking(false);
-    new Audio(url).play();
+    audio.current.play();
   }
 
   return (
